50b2ad5600f04ffeec9751753f1ed493c1938287
Bartek <bartosz.dadas@wp.pl>
2022-07-11 10:39:00 +0200

    initialize commit

---
+++ app/src/main/java/eu/tutorials/makeapp/ui/login/LoginViewModelFactory.java
@@ -0,0 +1,26 @@
+package eu.tutorials.makeapp.ui.login;
+
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.annotation.NonNull;
+
+import eu.tutorials.makeapp.data.LoginDataSource;
+import eu.tutorials.makeapp.data.LoginRepository;
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+public class LoginViewModelFactory implements ViewModelProvider.Factory {
+
+    @NonNull
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(LoginViewModel.class)) {
+            return (T) new LoginViewModel(LoginRepository.getInstance(new LoginDataSource()));
+        } else {
+            throw new IllegalArgumentException("Unknown ViewModel class");
+        }
+    }
+}
